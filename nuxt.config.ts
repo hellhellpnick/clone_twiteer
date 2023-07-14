f2cc203bdbe85838ccfa3a6bb27e01bd2bfb06df
Alex <hellhellpnick@gmail.com>
2023-07-14 18:15:05 +0300

    feat: added gh-page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,5 +16,10 @@ export default defineNuxtConfig({
         cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
         cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
         cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
-    }
+    },
+
+    app: {
+        baseURL: 'https://github.com/hellhellpnick/clone_twiteer' // baseURL: '/<repository>/'
+    },
+    
 })
